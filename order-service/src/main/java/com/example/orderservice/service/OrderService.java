4458ef379a1bf5150d35906a223346efbb07be2a
khang duy <nguyenduykhangcd12t4@gmail.com>
2023-06-23 00:38:33 +0700

    async communicate

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,8 +18,11 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
 
-    private final  OrderRepository orderRepository;
-    public void placeOrder(OrderRequest orderRequest){
+    private final OrderRepository orderRepository;
+
+    private final WebClient webClient;
+
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList().stream()
@@ -26,10 +30,21 @@ public class OrderService {
                 .toList();
         order.setOrderLineItems(orderLineItems);
 
-        orderRepository.save(order);
+        //Call Inventory Service to check if all items are in stock then place order
+        // bodyToMono  define the type of response data
+        //block() enable asynchronous call
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+        if(result)
+            orderRepository.save(order);
+        else
+            throw new IllegalArgumentException("Out of stock");
     }
 
-    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto){
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
